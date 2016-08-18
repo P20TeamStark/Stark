231a58cb2bc189c3e151645b22c956ccd857f095
Bob Jacobs <jacobsrj@hotmail.com>
2016-08-18 08:52:33 -0400

    Adding AzureADOauthGateway

--- rentMyJunk/rentMyJunk/Controllers/HomeController.cs
+++ rentMyJunk/rentMyJunk/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rentMyJunk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,23 @@ namespace rentMyJunk.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+
+            if (Request.IsAuthenticated)
+            {
+                AzureADOAuthGateway azureADOAuthGateway = new AzureADOAuthGateway
+                {
+                    AuthenticationAuthority = "https://login.windows.net",
+                    ClientId = "78316e80-f898-43f3-9823-f652d979dc0f",
+                    Key = "+9IaNb9TTwmAkAjayZheZY1ZQJYohWtPCpMrFRmoRcg=",
+                    Resource = "https://management.core.windows.net/",
+                    TenantId = "4ac2c945-49d5-4b59-8a70-a08dffe43dba"
+                };
+
+                string token = azureADOAuthGateway.GetOAuthToken();
+                int x = 0;
+
+                
+            }
 
             return View();
         }
